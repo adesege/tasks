b35d109c181e4afcb46a778ba73580265b41ca20
Fadojutimi Temitayo Olusegun <fadojutimitemitayo@gmail.com>
2017-07-31 21:18:00 +0100

    optimize artigeo()

--- src/main.js
+++ src/main.js
@@ -1,40 +1,34 @@
 'use strict'
 
-function checkGeometric(inputArray){
-    let result=false;
-    let firstTerm = inputArray[0];
-    let secondTerm = inputArray[1];
-    let ratio = secondTerm / firstTerm;
-    let i = 0;
-    let arrayLength = inputArray.length;
-    for(i=0; i < arrayLength-1; i++){
-        if(!Number.isInteger(inputArray[i])){ return -1; }
-        if(ratio == (inputArray[i+1]/inputArray[i])){
-            result= true;
-        }else{
-            return false;
-        }
-    }
-    return result;
-}
-
 function aritGeo(inputArray=false){
     /* Check if inputArray is an Arithmetric Progression or Geometric progression*/
 
     if(!inputArray){ return 0; }
     if(inputArray.length==0){ return 0; }
     if(!Array.isArray(inputArray)){ return 'undefined'; } // inputArray must be a typeof Array
-    let arithmetic=checkArithmetic(inputArray);
-    let geometric=checkGeometric(inputArray)
-    if(arithmetic===true){
-        return "Arithmetic";
-    }else{    
-        if(geometric===true){
-            return "Geometric";
+
+    let result=false;
+    let firstTerm = inputArray[0];
+    let secondTerm = inputArray[1];
+    let ratio = secondTerm / firstTerm;
+    let difference = secondTerm - firstTerm;
+    let arrayLength = inputArray.length;
+
+    for(let i=0; i < arrayLength-1; i++){
+
+        if(!Number.isInteger(inputArray[i])){ return -1; }
+
+        if(ratio == (inputArray[i+1]/inputArray[i])){
+            result= "Geometric";
         }else{
-            return -1;
+            if(difference == (inputArray[i+1]-inputArray[i])){
+                result= "Arithmetic";
+            }else{
+                return -1;
+            }
         }
     }
+    return result;
 }
 
 module.exports= {
